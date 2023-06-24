db94b2206ffaf2d1990e308d67ccf86f292534b6
Marco Haubrich <marcolino.haubrich@gmail.com>
2023-06-24 18:36:18 -0300

    refactor: :recycle: Mudada organizacao da HashList

--- est_dados/arvores/trabalho/hash_list.py
+++ est_dados/arvores/trabalho/hash_list.py
@@ -11,11 +11,35 @@ def _hash_function(value: str):
 class HashList:
     _list: list[HashNode | None] = [None] * 26
 
+    def __str__(self):
+        s = ""
+        for i in self._list:
+            if i is not None:
+                s += str(i)
+        return s
+
+    def __getitem__(self, value: str) -> Node | None:
+        return self._buscar(value)
+
+    def __len__(self):
+        return sum(len(i) for i in self._list if i is not None)
+
     def inserir(self, value: str):
         index = _hash_function(value)
         if self._list[index] is None:
             self._list[index] = HashNode(AVL())
         self._list[index].inserir(value)
+
+    def remover(self, value: str) -> bool:
+        index = _hash_function(value)
+        if self._list[index] is None:
+            return False
+        return self._list[index].remover(value)
+
+    def read_file(self, filename: str):
+        with open(filename, encoding="utf-8") as f:
+            for word in remove_pontuacao(f.read()).split():
+                self.inserir(word.strip())
 
     def print(self, reverse=False):
         lista = reversed(self._list) if reverse else self._list
@@ -23,28 +47,14 @@ class HashList:
             if i is not None:
                 i.print()
 
-    def to_list_with_letter(self, letter: str, reverse=False):
-        lista = reversed(self._list) if reverse else self._list
-        _lista: list[Node] = []
-
-        for i in lista:
+    def get_one_occur(self):
+        one_occur: list[Node] = []
+        for i in self._list:
             if i is not None:
-                if remove_acentuacao(
-                    remove_pontuacao(i.value.first().valor[0].lower())
-                ) == remove_acentuacao(remove_pontuacao(letter.lower())):
-                    _lista.extend(i.to_list(reverse))
-
-        return _lista
-
-    def to_list(self, reverse=False):
-        lista = reversed(self._list) if reverse else self._list
-        _lista: list[Node] = []
-
-        for i in lista:
-            if i is not None:
-                _lista.extend(i.to_list(reverse))
-
-        return _lista
+                node = i.value.pega_todos_com_uma_quantidade()
+                if node is not None:
+                    one_occur.extend(node)
+        return one_occur
 
     def get_most_occur(self):
         most_occur: Node | None = None
@@ -56,41 +66,29 @@ class HashList:
                         most_occur = node
         return most_occur
 
-    def __str__(self):
-        s = ""
-        for i in self._list:
+    def to_list(self, reverse=False):
+        lista = reversed(self._list) if reverse else self._list
+        _lista: list[Node] = []
+
+        for i in lista:
             if i is not None:
-                s += str(i)
-        return s
+                _lista.extend(i.to_list(reverse))
 
-    def __getitem__(self, value: str) -> Node | None:
-        return self._buscar(value)
+        return _lista
 
-    def read_file(self, filename: str):
-        with open(filename, encoding="utf-8") as f:
-            for word in remove_pontuacao(f.read()).split():
-                self.inserir(word.strip())
+    def to_list_with_letter(self, letter: str, reverse=False):
+        lista = reversed(self._list) if reverse else self._list
+        _lista: list[Node] = []
 
-    def get_one_occur(self):
-        one_occur: list[Node] = []
-        for i in self._list:
+        for i in lista:
             if i is not None:
-                node = i.value.pega_todos_com_uma_quantidade()
-                if node is not None:
-                    one_occur.extend(node)
-        return one_occur
+                if remove_acentuacao(remove_pontuacao(i.value.first().valor[0].lower())) == remove_acentuacao(remove_pontuacao(letter.lower())):
+                    _lista.extend(i.to_list(reverse))
+
+        return _lista
 
     def _buscar(self, value: str):
         index = _hash_function(value)
         if self._list[index] is None:
             return None
         return self._list[index][value]
-
-    def remover(self, value: str) -> bool:
-        index = _hash_function(value)
-        if self._list[index] is None:
-            return False
-        return self._list[index].remover(value)
-
-    def __len__(self):
-        return sum(len(i) for i in self._list if i is not None)
